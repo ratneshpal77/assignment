b5fe2a0c670cf1a8ce04e16f5482b4add2abbfdd
ratneshpal77 <ratneshpal776@gmail.com>
2026-06-20 16:00:16 +0530

    db

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -1,16 +1,49 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI);
+
+//     console.log("MongoDB Connected");
+//   } catch (error) {
+//     console.log("Database Error:", error);
+    
+//   }
+// };
+
+// export default connectDB;
+
+
+
+
+
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+const MONGO_URI = process.env.MONGO_URI;
 
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.log("Database Error:", error);
-    
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
+
+
+
+
 
 
