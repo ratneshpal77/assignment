3316a88ee1dc94df1ade66c3c981b4503b79e43c
ratneshpal77 <ratneshpal776@gmail.com>
2026-06-20 14:27:18 +0530

    frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,33 +2,22 @@ import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import eventRoutes from "./routes/event.routes.js";
 
-
-
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
-import cors from "cors";
-
-app.use(
-  cors({
-    origin: [
-      "https://assignment-vckh.vercel.app/"
-    ],
-    credentials: true,
-  })
-);
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser()); //middleware
 app.use(cors());
 
-app.use(express.static("frontend/dist"));
-
-
+app.use(
+  cors({
+    origin: ["https://assignment-sigma-rosy.vercel.app/"],
+    credentials: true,
+  }),
+);
 
 app.use("/api/user", userRoutes);
-app.use("/api", eventRoutes)
-
+app.use("/api", eventRoutes);
 
 export default app;
