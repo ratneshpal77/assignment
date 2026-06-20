57e1a190f865c2bfce9f5f9c08906b083782c8c8
ratneshpal77 <ratneshpal776@gmail.com>
2026-06-20 12:40:00 +0530

    fix express wildcard route

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use(express.static("frontend/dist"));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(
     path.resolve("frontend/dist/index.html")
   );
