9f4540ca8a1d06ddf2689b7d9c886a5b7affb6b4
ratneshpal77 <ratneshpal776@gmail.com>
2026-06-20 15:36:57 +0530

    change

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,38 @@
+// import express from "express";
+// import app from "./src/app.js";
+// import connectDB from "./src/db/db.js";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// connectDB();
+
+// const PORT = 3000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`);
+// });
+
+
+
 import express from "express";
 import app from "./src/app.js";
 import connectDB from "./src/db/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB(); // ✅ WAIT FOR DB FIRST
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  } catch (error) {
+    console.log("DB Connection Failed:", error);
+  }
+};
+
+startServer();
