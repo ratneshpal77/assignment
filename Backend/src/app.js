9a3d9872c29f18931cd1706c671fa37edb2eab2e
ratneshpal77 <ratneshpal776@gmail.com>
2026-06-20 12:00:03 +0530

    app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import userRoutes from "./routes/user.routes.js";
 import eventRoutes from "./routes/event.routes.js";
+import path from "path";
 
 
 import cookieParser from "cookie-parser";
@@ -10,6 +11,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); //middleware
 app.use(cors());
+
+app.use(express.static("frontend/dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.resolve("frontend/dist/index.html")
+  );
+});
 
 app.use("/api/user", userRoutes);
 app.use("/api", eventRoutes)
